8c9e0dc88198478673a4e895d421a68c4e17ed8f
ferdjanidhirar <ferdjanidhirar@gmail.com>
2025-04-30 10:48:48 +0100

    Create bmi_form.php

---
+++ bmi_project/app/views/bmi_form.php
@@ -0,0 +1,6 @@
+<form method="POST">
+    <input type="text" name="name" placeholder="Name" required>
+    <input type="number" step="any" name="weight" placeholder="Weight (kg)" required>
+    <input type="number" step="any" name="height" placeholder="Height (cm)" required>
+    <button type="submit">Calculate BMI</button>
+</form>
