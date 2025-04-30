63b45c2160c3ce9752acb9d7f6d32101e5a0ee06
ferdjanidhirar <ferdjanidhirar@gmail.com>
2025-04-30 10:53:47 +0100

    Create database.php

---
+++ bmi_project/config/database.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'bmi_db';
+$user = 'root';
+$pass = '';
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
